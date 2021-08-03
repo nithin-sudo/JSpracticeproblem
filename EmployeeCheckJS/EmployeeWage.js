56f2b1025d852d0154d9c189e46498339d6849f2
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-08-03 22:27:20 +0530

    Refactor: Keeping the code inside the function.

--- EmployeeCheckJS/EmployeeWage.js
+++ EmployeeCheckJS/EmployeeWage.js
@@ -14,18 +14,18 @@ const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 
-let empHrs = 0;
-employeeCheck =  Math.floor((Math.random() * 10) % 3);
-switch (employeeCheck){
-    case IS_PART_TIME : 
-            empHrs = PART_TIME_HOURS;
-            break;
-    case IS_FULL_TIME :
-        empHrs = FULL_TIME_HOURS;
-        break;  
-     default :
-     empHrs = 0;               
+function getWorkingHours(employeeCheck){
+        switch (employeeCheck){
+            case IS_PART_TIME : 
+                return PART_TIME_HOURS;
+            case IS_FULL_TIME :
+                return FULL_TIME_HOURS;
+            default :
+                return  0;               
+        }
 }
-
+employeeCheck =  Math.floor((Math.random() * 10) % 3);
+let empHrs = 0
+empHrs=getWorkingHours(employeeCheck)
 let empWage = empHrs * WAGE_PER_HOUR;
 console.log("Employee Wage :"+ empWage)
