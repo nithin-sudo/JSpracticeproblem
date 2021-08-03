0caf0aa6c8da35dd11a7b94a8f96fb1cc02cd312
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-08-03 22:42:22 +0530

    Refactor:code and calculating wage for number of working days.

--- EmployeeCheckJS/EmployeeWage.js
+++ EmployeeCheckJS/EmployeeWage.js
@@ -7,7 +7,7 @@ const IS_ABSENT = 0;
         console.log("Employee is Present!");
     }
 
-//UC2
+//UC3
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
@@ -28,4 +28,14 @@ employeeCheck =  Math.floor((Math.random() * 10) % 3);
 let empHrs = 0
 empHrs=getWorkingHours(employeeCheck)
 let empWage = empHrs * WAGE_PER_HOUR;
+console.log("Employee Wage :"+ empWage)
+
+//UC4
+const NUM_OF_WORKING_DAYS = 2
+empHrs = 0
+for (let day = 0; day < NUM_OF_WORKING_DAYS; day++){
+    employeeCheck =  Math.floor((Math.random() * 10) % 3);
+    empHrs += getWorkingHours(employeeCheck)
+}
+ empWage = empHrs * WAGE_PER_HOUR;
 console.log("Employee Wage :"+ empWage)
