0c485c869e571437cc0b8a8171684413f32012b8
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-08-04 19:32:00 +0530

    Added:taking the number as input and printing in word.

---
+++ ifelseifproblems/NumberToWord.js
@@ -0,0 +1,25 @@
+const prompt = require("prompt-sync")();
+let number = prompt("Enter a number from 0-9 : ")
+if (number == 0) {
+    console.log("Zero");
+}else if (number == 1) {
+    console.log("One");
+}else if (number == 2) {
+    console.log("Two");
+}else if (number == 3) {
+    console.log("Three");
+}else if (number == 4) {
+    console.log("Four");
+}else if (number == 5) {
+    console.log("Five");
+}else if (number == 6) {
+    console.log("Six");
+}else if (number == 7) {
+    console.log("Seven");
+}else if (number == 8) {
+    console.log("Eight");
+}else if (number == 9) {
+    console.log("Nine");
+}else {
+    console.log("Entered wrong input!")
+}
