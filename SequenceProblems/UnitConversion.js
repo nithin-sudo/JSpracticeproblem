e34b38df5659cdee5d902ef286ec49e56c83ce29
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-08-04 16:53:55 +0530

    Added:converting different types of measurements.

---
+++ SequenceProblems/UnitConversion.js
@@ -0,0 +1,6 @@
+let inchToFeet = 42/12
+console.log("The 42 inch in feet  = "+inchToFeet)
+let rectangularAreaInMetres =  (60*40) * 0.092903
+console.log("Area of rectangular plot is "+rectangularAreaInMetres)
+let areaOf25Plots = rectangularAreaInMetres * 25
+console.log("Area of 25 plots in acres = "+areaOf25Plots * 0.000247105)
